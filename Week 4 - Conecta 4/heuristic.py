866d5f8564714154a018afcaea404a4340402a09
Luis Zoran <luiszoran@gmail.com>
2016-04-26 15:52:18 +0100

    Columns heuristic needs fix

--- Week 4 - Conecta 4/heuristic.py
+++ Week 4 - Conecta 4/heuristic.py
@@ -3,3 +3,24 @@ from random import randint
 
 def randomHeuristic(state):
     return randint(0, 100)
+
+def columnsHeuristic(state):
+    board = state.board
+    h, v = 7, 6
+    lista = []
+    for x in range(1, h + 1):
+        for y in range(v, 0, -1):
+            ficha = board.get((x, y), '.')
+            if ficha == ".":
+                continue
+            if ficha == "O":
+                lista.append(-1)
+                break
+
+            if ficha == "X":
+                j = y - 1
+                while board.get((x, j), '.') == "X":
+                    j -= 1
+                lista.append((y - j) * 2)
+                break
+    return max(lista)

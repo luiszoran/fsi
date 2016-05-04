4ba03d051053fec07e3661dd6763c4197f996880
Luis Zoran <luiszoran@gmail.com>
2016-05-04 12:22:08 +0100

    Now checking legal moves to count players and computers chips and space above

--- Week 4 - Conecta 4/heuristic.py
+++ Week 4 - Conecta 4/heuristic.py
@@ -4,23 +4,31 @@ from random import randint
 def randomHeuristic(state):
     return randint(0, 100)
 
+
 def columnsHeuristic(state):
     board = state.board
-    h, v = 7, 6
-    lista = []
-    for x in range(1, h + 1):
-        for y in range(v, 0, -1):
-            ficha = board.get((x, y), '.')
-            if ficha == ".":
-                continue
-            if ficha == "O":
-                lista.append(-1)
-                break
+    moves = legal_moves(state)
+    he = 0
+    for move in moves:
+        x, y = move
+        y -= 1
+        while board.get((x, y), '.') == "X":
+            y -= 1
+            he += 50
 
-            if ficha == "X":
-                j = y - 1
-                while board.get((x, j), '.') == "X":
-                    j -= 1
-                lista.append((y - j) * 2)
-                break
-    return max(lista)
+        x, y = move
+        y -= 1
+        while board.get((x, y), '.') == "O":
+            y -= 1
+            he -= 25
+
+        x, y = move
+        he += (7-y)*50
+    return he
+
+
+def legal_moves(state):
+    "Legal moves are any square not yet taken."
+
+    return [(x, y) for (x, y) in state.moves
+            if y == 1 or (x, y - 1) in state.board]

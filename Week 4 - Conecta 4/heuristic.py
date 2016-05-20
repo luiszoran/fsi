36ca26e035783b1edaed8c114a98d7951a51108e
Luis Zoran <luiszoran@gmail.com>
2016-05-20 20:19:17 +0100

    Even more refactoring

--- Week 4 - Conecta 4/heuristic.py
+++ Week 4 - Conecta 4/heuristic.py
@@ -16,83 +16,27 @@ def calculateHeuristic(state, player):
     moves = legal_moves(state)
     he = 0
     for move in moves:
-        he += comprobarFilas(board, move, player)
-        he += comprobarColumnas(board, move, player)
-        he += comprobarDiagonales(board, move, player)
+        # Comprobar Filas
+        he += comprobarLinea(board, move, player, -1, 0)
+        he += comprobarLinea(board, move, player, 1, 0)
+        # Comprobar columnas
+        he += comprobarLinea(board, move, player, 0, -1)
+        he += comprobarLinea(board, move, player, 0, 1)
+        # Comprobar diagonales
+        he += comprobarLinea(board, move, player, -1, 1)
+        he += comprobarLinea(board, move, player, 1, -1)
+        he += comprobarLinea(board, move, player, 1, 1)
+        he += comprobarLinea(board, move, player, -1, -1)
     return he
 
 
-def comprobarFilas(board, move, player):
-    he = 0
-    he += comprobarFila(board, move, player, -1)
-    he += comprobarFila(board, move, player, 1)
-    return he
-
-
-def comprobarFila(board, move, player, desplazamiento):
-    he = 0
-    x, y = move
-    x += desplazamiento
-    count = 0
-    while x > 0:
-        if count == 4:
-            he += 5000
-            return he
-        if board.get((x, y), '.') == player:
-            he += 50
-            count += 1
-        elif board.get((x, y), '.') == ".":
-            he += 25
-        else:
-            break
-        x += desplazamiento
-        count += 1
-    return he
-
-
-def comprobarColumnas(board, move, player):
-    he = 0
-    he += comprobarColumna(board, move, player, -1)
-    he += comprobarColumna(board, move, player, 1)
-    return he
-
-
-def comprobarColumna(board, move, player, desplazamiento):
-    he = 0
-    x, y = move
-    y += desplazamiento
-    count = 0
-    while y > 0:
-        if count == 4:
-            he += 5000
-            return he
-        if board.get((x, y), '.') == player:
-            he += 50
-            count += 1
-        elif board.get((x, y), '.') == ".":
-            he += 25
-        else:
-            break
-        y += desplazamiento
-    return he
-
-
-def comprobarDiagonales(board, move, player):
-    he = 0
-    he += comprobarDiagonal(board, move, player, -1, 1)
-    he += comprobarDiagonal(board, move, player, 1, -1)
-    he += comprobarDiagonal(board, move, player, 1, 1)
-    he += comprobarDiagonal(board, move, player, -1, -1)
-    return he
-
-
-def comprobarDiagonal(board, move, player, desplazamientoX, desplazamientoY):
+def comprobarLinea(board, move, player, desplazamientoX, desplazamientoY):
     he = 0
     x, y = move
     x += desplazamientoX
     y += desplazamientoY
     count = 0
-    while y > 0 and y < 7 and x > 0 and x < 8:
+    while 0 < y < 7 and 0 < x < 8:
         if count == 4:
             he += 5000
             return he
@@ -106,6 +50,7 @@ def comprobarDiagonal(board, move, player, desplazamientoX, desplazamientoY):
         x += desplazamientoX
         y += desplazamientoY
     return he
+
 
 def legal_moves(state):
     "Legal moves are any square not yet taken."
